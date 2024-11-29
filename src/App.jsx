5cb6c48226c8b4adbcf767df25ae3fab8d5fe554
Boris Khenzykhenov <123456789+odin@users.noreply.github.com>
2024-11-29 13:14:16 -0500

    lesson 4 -lifting state and props handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,37 @@
-import './App.css';
 import React, {useState} from 'react';
+import './App.css';
 import TodoList from './components/TodoList';
 import AddTodoForm from './components/AddTodoForm';
-import TodoListItem from './components/TodoListItem';
+
 
 const Heading = () => <h1>Todo list</h1>
 
 const App = () => {
-  const todoList = [
-    {
-      id: 1, 
-      title: "Complete assignment"
-    },
-    {
-      id: 2, 
-      title: "Read the book"
-    },
-    {
-      id: 3, 
-      title: "Watch the video class"
-    },
-  ];
-  // creaon
-  const [newTodo, setNewTodo] = useState("");
+  // const todoList = [
+  //   {
+  //     id: 1, 
+  //     title: "Complete assignment"
+  //   },
+  //   {
+  //     id: 2, 
+  //     title: "Read the book"
+  //   },
+  //   {
+  //     id: 3, 
+  //     title: "Watch the video class"
+  //   },
+  // ];
+  // creating state
+  
+  const [todoList, setTodoList] = useState([]);
+  const addTodo = (newTodo) => {
+    setTodoList([newTodo, ...todoList]);
+  }
   return (
     <div>
       <Heading/>
-      <AddTodoForm onAddTodo={setNewTodo}/>
-      <p>
-        New todo is {newTodo}
-      </p>
-      <TodoList list={todoList} />
+      <AddTodoForm onAddTodo={addTodo}/>
+      <TodoList todoList={todoList} />
     </div>
   )
 }
